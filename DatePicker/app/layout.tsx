887ec51bde3c7715971ad9d90b9131b9eb2a3902
satya <satyjeet.singh12005@gmail.com>
2025-07-21 01:57:24 +0530

    error in layout file fixed

--- DatePicker/app/layout.tsx
+++ DatePicker/app/layout.tsx
@@ -1,12 +1,10 @@
-"use client"
-
 import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'date picker application',
+  description: 'created for the pearl thoughts',
+  generator: 'satya',
 }
 
 export default function RootLayout({
